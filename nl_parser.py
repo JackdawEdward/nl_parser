340d3d5b8789ad3774d53416458a94fb4e92fb1f
JackdawEdward <1349586203@qq.com>
2024-04-09 17:40:35 +0800

    test apart from parser code, add port feature and fix parser function

--- nl_parser.py
+++ nl_parser.py
@@ -53,87 +53,131 @@ class nl_parser:
         m1 = n.split(';')
 
         #print("len of m1: {}".format(len(m1)))
-        
+        module = False
+        port = False
+        wire = False
         for i in range(len(m1)):
             #module name
-            ret = re.match(r'^module\s+(\w+)\s+', m1[i], re.S)
-            if ret:
-                module_name = ret.group(1)
-                module_temp = t_module(module_name)
-                self.designs.append(module_temp)
-                continue
+            if not module:    
+                ret = re.match(r'^module\s+(\w+)\s+', m1[i], re.S)
+                if ret:
+                    module_name = ret.group(1)
+                    module_temp = t_module(module_name)
+                    self.designs.append(module_temp)
+                    module = True
+                    continue
 
             #port
-            found = 0
             m1_temp = re.sub(r'\n', '', m1[i], re.S)
-            ret = re.match(r'^(input|output|inout)\s+\[(\d+):(\d+)\]\s+(\w+)', m1_temp, re.S)
-            if ret:
-                port_direction = ret.group(1)
-                port_msb = ret.group(2)
-                port_lsb = ret.group(3)
-                port_name = ret.group(4)
-                found = 1
-                
-            else:
-                ret = re.match(r'^(input|output|inout)\s+\[(\d+)\]\s+(\w+)', m1_temp, re.S)
+            m1_temp = re.sub(r'\s+',' ',m1_temp)
+            if not port:    
+                ret = re.match(r'^(input|output|inout)\s+\[(\d+):(\d+)\]\s+(\w+)', m1_temp, re.S)
                 if ret:
                     port_direction = ret.group(1)
-                    port_msb = ret.group(2)
-                    port_lsb = ret.group(2)
-                    port_name = ret.group(3)
-                    found = 1
-                    
+                    if int(ret.group(2)) > int(ret.group(3)):
+                        port_msb = int(ret.group(2))
+                        port_lsb = int(ret.group(3))
+                    else:
+                        port_msb = int(ret.group(3))
+                        port_lsb = int(ret.group(2))
+                    port_name = ret.group(4)
+                    port_temp = t_port()
+                    port_temp.new_port(port_name, port_direction, "wire", port_msb, port_lsb)
+                    try:
+                        module_temp.add_port(port_temp)
+                    except UnboundLocalError as e:
+                        print("line info unparserable ", m[i])
+                    continue
+
                 else:
-                    ret = re.match(r'^(input|output|inout)\s+(\w+)', m1_temp, re.S)
+                    ret = re.match(r'^(input|output|inout)\s+\[(\d+)\]\s+(\w+)', m1_temp, re.S)
                     if ret:
                         port_direction = ret.group(1)
-                        port_msb = '0'
-                        port_lsb = '0'
-                        port_name = ret.group(2)
-                        found = 1
+                        port_msb = ret.group(2)
+                        port_lsb = ret.group(2)
+                        port_name = ret.group(3)
+                        port_temp = t_port()
+                        port_temp.new_port(port_name, port_direction, "wire", port_msb, port_lsb)
+                        try:
+                            module_temp.add_port(port_temp)
+                        except UnboundLocalError as e:
+                            print("line info unparserable ", m[i])
+                        continue
 
-            if found == 1:
-                port_msb = int(port_msb)
-                port_lsb = int(port_lsb)
+                    else:
+                        ret = re.match(r'^\s*(input|output|inout)\s+(\w+)', m1_temp, re.S)
+                        rep = re.match(r'^\s*(input|output|inout)\s+(\w+)(,\s*\w+)+', m1_temp)
+                        if ret and not rep:
+                            port_direction = ret.group(1)
+                            port_msb = 0
+                            port_lsb = 0
+                            port_name = ret.group(2)
+                            port_temp = t_port()
+                            port_temp.new_port(port_name, port_direction, "wire", port_msb, port_lsb)
+                            try:
+                                module_temp.add_port(port_temp)
+                            except UnboundLocalError as e:
+                                print("line info unparserable ", m[i])
+                            continue
+                        elif rep:
+                            port_direction = ret.group(1)
+                            port_msb = 0
+                            port_lsb = 0
+                            m2 = re.match(r'^\s*(input|output|inout)\s+((\w+)(,\s*\w+)+)', m1_temp).group(2)
+                            m2 = re.sub(r'\s', '', m2)
+                            m2_temp = m2.split(',')
+                            for m2port in m2_temp:
+                                port_temp = t_port()
+                                port_temp.new_port(m2port, port_direction, "wire", port_msb, port_lsb)
+                                try:
+                                    module_temp.add_port(port_temp)
+                                except UnboundLocalError as e:
+                                    print("line info unparserable ", m[i])
+                            continue
+
                 
-                port_temp = t_port()
-                port_temp.new_port(port_name, port_direction, "wire", port_msb, port_lsb)
-                module_temp.add_port(port_temp)
-            
-                for i in range(port_lsb, port_msb):
-                    wire_temp = t_wire("{}{}".format(port_name, i))
-                    wire_temp.add_conn(None, "{}[{}]".format(port_name, i))
-                    module_temp.add_wire(wire_temp)
-
-                continue
-
             #wire
-            ret = re.match(r'^wire\s+\[(\d+):(\d+)\]\s+(\w+)', m1_temp, re.S)
-            if ret:
-                port_direction = "input"
-                port_msb = ret.group(1)
-                port_lsb = ret.group(2)
-                port_name = ret.group(3)
-                
-                port_msb = int(port_msb)
-                port_lsb = int(port_lsb)
-                
-                for i in range(port_lsb, port_msb):
-                    wire_temp = t_wire("{}{}".format(port_name, i))
-                    wire_temp.add_conn(None, "{}[{}]".format(port_name, i))
-                    module_temp.add_wire(wire_temp)
-                    
-                continue
+            if not wire:
+                ret = re.match(r'^wire\s+\[(\d+):(\d+)\]\s+(\w+)', m1_temp, re.S)
+                rep = re.match(r'^\s*wire\s+(,\s*\w+)+',m1_temp)
+                if ret and not rep:
+                    if int(ret.group(1)) > int(ret.group(2)):
+                        wire_msb = int(ret.group(1))
+                        wire_lsb = int(ret.group(2))
+                    else:
+                        wire_msb = int(ret.group(2))
+                        wire_lsb = int(ret.group(1))
+                    wire_name = ret.group(3)
+                    port = True
+                    for i in range(wire_lsb, wire_msb+1):
+                        wire_temp = t_wire("{}[{}]".format(wire_name, i))
+                        # wire_temp.add_conn(None, "{}[{}]".format(port_name, i))
+                        module_temp.add_wire(wire_temp)
+                    continue
+                elif rep:
+                    port = True
+                    m2 = re.match(r'^\s+wire\s+(((\w+)(,\s*\w+)+))', m1_temp).group(2)
+                    m2 = re.sub(r'\s','',m2)
+                    m2_temp = m2.split(',')
+                    for m2wire in m2_temp:
+                        wire_temp = t_wire(m2wire)
+                        module_temp.add_wire(wire_temp)
+                    continue
 
             #endmodule
             ret = re.match(r'^\s*endmodule', m1_temp, re.S)
             if ret:
+                module = False
+                port = False
+                wire = False
                 continue
                     
             #inst
             ret = re.match(r'^\s*(\w+)\s+(\w+)\s+\((.*)\)\s*$', m1_temp, re.S)
             if ret:
                 #print(m1_temp)
+                wire = True
+
                 mod_name = ret.group(1)
                 inst_name = ret.group(2)
                 port_map_s = ret.group(3)
@@ -150,7 +194,10 @@ class nl_parser:
                     #print(r)
                     if r:
                         inst_temp.add_port_map(r.group(1), r.group(2))
-                        if module_temp.exist_wire(r.group(2)):
+                        if module_temp.exist_port(r.group(2)):
+                            i = module_temp.find_port(r.group(2))
+                            module_temp.ports[i].add_conn(r.group(2), inst_name, r.group(1))
+                        elif module_temp.exist_wire(r.group(2)):
                             i = module_temp.find_wire(r.group(2))
                             module_temp.wires[i].add_conn(inst_name, r.group(1))
                         else:
@@ -165,12 +212,7 @@ class nl_parser:
                 print(m1_temp)
 
                 
-if __name__ == '__main__':
-    parser = nl_parser()
-    parser.read_netlist("test.v")
 
-    #parser.designs[0].print_this()
-    parser.designs[0].get_wires()
     
     
             
